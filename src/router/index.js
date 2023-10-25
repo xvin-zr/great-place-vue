7aa0c9b83e028369bf86a51a28ac757b5335d87a
“szl” <2505147926@qq.com>
2023-10-25 11:43:46 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import HelloWorld from "../components/HelloWorld.vue";
+// import HelloWorld from "../components/HelloWorld.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import hello from "../views/hello.vue";
-import Login from "../views/Login.vue";
-import SignUp from "../views/SignUp.vue";
+// import hello from "../views/hello.vue";
+// import Login from "../views/Login.vue";
+// import SignUp from "../views/SignUp.vue";
 
 const routes = [
-  { path: "/", name: "Login", component: Login },
+  { path: "/", name: "Login", component: ()=>import('../views/Login.vue')},
   {
     path: "/signup",
     name: "SignUp",
-    component: SignUp,
+    component: ()=>import('../views/SignUp.vue'),
   },
 ];
 const router = createRouter({
