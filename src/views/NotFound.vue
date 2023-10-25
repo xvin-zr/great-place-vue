19fb0f5250281f85c9cf46a2616361fd64db63cb
xvin <xvin@bupt.edu.cn>
2023-10-25 15:23:00 +0800

    improve 404 not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,17 @@
+<script setup>
+import { reactive } from "vue";
+
+const playlist = reactive([
+  "https://embed.music.apple.com/cn/album/%E6%88%91%E6%98%AF%E4%B8%80%E5%8F%AA%E5%B0%8F%E5%B0%8F%E9%B8%9F/1329432176?i=1329432678",
+  "https://embed.music.apple.com/cn/album/%E7%AB%A5%E5%B9%B4/1318097678?i=1318098161",
+  "https://embed.music.apple.com/cn/album/%E7%AA%81%E7%84%B6%E7%9A%84%E8%87%AA%E6%88%91/840075970?i=840076048",
+]);
+</script>
+
 <template>
   <h1>404 Not Found. 😅</h1>
   <h1 class="h1-2">
-    Tired of searching places? <br />
+    Tired of searching for places? <br />
     Let's listen to a song. 🎵
   </h1>
   <iframe
@@ -15,9 +25,8 @@
       background: transparent;
     "
     sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-    src="https://embed.music.apple.com/cn/album/%E6%88%91%E6%98%AF%E4%B8%80%E5%8F%AA%E5%B0%8F%E5%B0%8F%E9%B8%9F/1329432176?i=1329432678"
+    :src="playlist[Math.floor(Math.random() * playlist.length)]"
   ></iframe>
-  
 </template>
 
 <style scoped>
